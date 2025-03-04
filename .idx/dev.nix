91f4a055441cbd11fc457c4b5866ba96161b319e
adam2802002 <adam2802002@gmail.com>
2025-03-04 14:03:16 +0000

    add idx conf

---
+++ .idx/dev.nix
@@ -0,0 +1,26 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.bun
+  ];
+  idx.extensions = [
+    
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
