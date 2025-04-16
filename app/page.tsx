b79042bf2e4dd2df23ef5c4ffd396b994fbe67b3
agripaa <agrieva.xananda08@gmail.com>
2025-04-16 09:36:59 +0700

    update: responsive done

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ import Link from "next/link";
 export default function Home() {
   const [scrolled, setScrolled] = useState(false);
   const [activeTab, setActiveTab] = useState("general");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   if (analytics) {
     logEvent(analytics, "page_view", {
@@ -92,32 +93,40 @@ export default function Home() {
     <>
       {/* Navbar */}
       <nav
-          className={`w-full z-20 top-0 start-0 fixed text-white transition-all duration-300 ${
-            scrolled ? "bg-[#08023D] shadow-md" : "bg-transparent"
-          }`}
-        >
+        className={`w-full z-20 top-0 start-0 fixed text-white transition-all duration-300 ${
+          scrolled || isMenuOpen ? "bg-[#08023D] shadow-md" : "bg-transparent"
+        }`}
+      >
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
           <Link href="/">
-            <Image
-              src={nav}
-              alt="Logo"
-              width={100}
-              height={56}
-            />
+            <Image src={nav} alt="Logo" width={100} height={56} />
           </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <Link href={"/auth/login"}>
+            <Link href={"/auth/login"} className="hidden md:block">
               <Button className="w-auto bg-[#4A90E2] px-6 rounded-xl text-white hover:bg-[#3a78bd]">
                 Register Now
               </Button>
             </Link>
-            <Button variant="outline" size="icon" className="md:hidden">
+            {/* Burger Menu Icon */}
+            <Button
+              variant="outline"
+              size="icon"
+              className="md:hidden border-none bg-transparent"
+              onClick={() => setIsMenuOpen(!isMenuOpen)} // Toggle menu on click
+            >
               <span className="sr-only">Open main menu</span>
-              <Menu />
+              {isMenuOpen ? (
+                <FaTimes style={{ width: '28px', height: '28px' }} />
+              ) : (
+                <Menu style={{ width: '28px', height: '28px' }} />
+              )}
             </Button>
           </div>
+          {/* Navbar links for desktop */}
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+              isMenuOpen ? "block" : "hidden"
+            }`} // Show menu when isMenuOpen is true
             id="navbar-sticky"
           >
             <ul className="flex flex-col p-4 md:p-0 mt-2 font-medium text-white md:flex-row md:mt-0">
@@ -136,6 +145,13 @@ export default function Home() {
                   <Link href="/">About Us</Link>
                 </Button>
               </li>
+              <li className="block md:hidden mt-2">
+                <Link href={"/auth/login"}>
+                  <Button className="w-auto bg-[#4A90E2] px-6 rounded-xl text-white hover:bg-[#3a78bd]">
+                    Register Now
+                  </Button>
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
@@ -144,34 +160,24 @@ export default function Home() {
     <div className="relative w-full min-h-screen overflow-hidden bg-[linear-gradient(to_bottom,_#08023D_1%,_#2135DB_49%,_white_100%)] text-black pt-[100px]">
       {/* Main Content */}
       <main className="relative z-10 max-w-screen-xl mx-auto px-4">
-        <section
-          id="hero"
-          className="relative flex flex-col md:flex-row gap-10 mt-20 md:mt-32 w-full"
-        >
-          {/* Gambar kanan */}
-          <div className="relative ml-auto">
-            <Image
-              src={maskot}
-              alt="Hero Illustration"
-              width={550}
-              height={550}
-            />
-          </div>
+      <section
+        id="hero"
+        className="relative flex flex-col items-center lg:flex-row gap-10 mt-20 lg:mt-32 w-full"
+      >
+        {/* Text kiri */}
+        <div className="flex flex-col justify-center h-full lg:w-[626px] px-6 lg:px-0">
+          <h4 className="text-3xl text-white font-medium drop-shadow-md">Welcome</h4>
+          <h1 className="text-5xl md:text-6xl lg:text-8xl font-bold lg:leading-[90px] text-white drop-shadow-md">
+            Gunadarma <br />
+            Code Week
+          </h1>
 
-          {/* Text kiri */}
-          <div className="md:absolute md:left-0 gap-3 flex flex-col justify-center h-full md:w-[626px]">
-            <h4 className="text-3xl text-white font-medium drop-shadow-md">Wellcome</h4>
-            <h1 className="text-3xl md:text-8xl font-bold md:leading-[90px] text-white drop-shadow-md">
-              Gunadarma <br />
-              Code Week
-            </h1>
+          <p className="mt-2 md:text-xl lg:text-2xl text-white drop-shadow">
+            GCW menghadirkan seminar, hackathon, dan kompetisi pemrograman.
+            Daftar sekarang untuk mengasah skill & berprestasi!
+          </p>
 
-            <p className="mt-2 md:text-2xl text-white drop-shadow">
-              GCW menghadirkan seminar, hackathon, dan kompetisi pemrograman.
-              Daftar sekarang untuk mengasah skill & berprestasi!
-            </p>
-
-            <div className="flex mt-4 gap-4">
+          <div className="flex mt-4 gap-4">
             <Button
               size="lg"
               className="px-6 py-6 rounded-xl text-white font-medium text-lg flex items-center gap-2 
@@ -179,16 +185,28 @@ export default function Home() {
             >
               Register Now <FaArrowRight className="text-sm" />
             </Button>
-            </div>
           </div>
-        </section>
+        </div>
+
+        {/* Gambar kanan */}
+        <div className="relative w-full lg:w-1/2">
+          <Image
+            src={maskot}
+            alt="Hero Illustration"
+            width={550}
+            height={550}
+            className="mx-auto lg:mx-0"
+          />
+        </div>
+      </section>
+
 
         <section className="mt-32 w-full flex flex-col items-center justify-center text-center text-black gap-6 min-h-screen">
           <h2 className="text-2xl md:text-4xl font-semibold mb-5">Let the countdown begin</h2>
-          <div className="flex gap-4 w-full justify-center items-center relative">
+          <div className="flex flex-col md:flex-row gap-4 w-full justify-center items-center relative">
 
             {/* Maskot Hi di sebelah kiri */}
-            <div className="absolute -left-15 md:-left-14 -top-10">
+            <div className="absolute hidden lg:block -left-15 md:-left-14 -top-10">
               <Image
                 src={maskotHi}
                 alt="Maskot Hi"
@@ -209,7 +227,7 @@ export default function Home() {
                 key={index}
                 className="bg-white w-[230px] h-[230px] backdrop-blur-md text-[#1A73E8] px-6 py-4 rounded-xl shadow-md flex flex-col items-center justify-center"
               >
-                <h1 className="text-5xl md:text-8xl font-semibold">{item.value}</h1>
+                <h1 className="text-7xl md:text-8xl font-semibold">{item.value}</h1>
                 <p className="text-base mt-2 font-medium">{item.label}</p>
               </div>
             ))}
@@ -217,7 +235,7 @@ export default function Home() {
         </section>
       </main>
     </div>
-    <main className="relative z-10 max-w-screen-xl mx-auto px-4">
+    <main className="relative z-10 max-w-screen-xl mx-auto px-4 mt-32">
       <section className="w-full flex flex-col gap-16 items-center justify-center text-black min-h-screen">
           {/* Header */}
           <div className="flex flex-col md:flex-row w-full justify-between items-start md:items-center gap-6">
@@ -232,7 +250,7 @@ export default function Home() {
           {/* Timeline content */}
           <div className="relative w-full">
             {/* Garis biru di tengah tanggal */}
-            <div className="absolute top-[72.5%] left-1/2 transform -translate-x-1/2 w-10/12 h-1 bg-[#4285F4] z-0" />
+            <div className="absolute top-[72.5%] hidden md:block left-1/2 transform -translate-x-1/2 w-10/12 h-1 bg-[#4285F4] z-0" />
 
             {/* Cards */}
             <div className="relative z-10 grid grid-cols-1 md:grid-cols-4 gap-10 w-full">
@@ -353,50 +371,51 @@ export default function Home() {
           </div>
         </main>
       </div>
-        <main className="relative z-10 max-w-screen-xl mx-auto px-4">
-          <section className=" w-full flex flex-col items-center justify-center text-center text-black gap-6">
-            <div className='bg-white/60 p-12 rounded-md border-gray-300 border-2'>
-              <h2 className="text-center text-3xl md:text-4xl font-bold text-black mb-10">
-                Got Questions? <span className="text-[#4285F4]">We&apos;ve Got Answers!</span>
-              </h2>
+      <main className="relative z-10 max-w-screen-xl mx-auto px-4">
+        <section className="w-full flex flex-col items-center justify-center text-center text-black gap-6">
+          <div className="bg-white/60 p-6 md:p-12 rounded-md border-gray-300 border-2 w-full">
+            <h2 className="text-center text-2xl sm:text-3xl md:text-4xl font-bold text-black mb-6 sm:mb-10">
+              Got Questions? <span className="text-[#4285F4]">We&apos;ve Got Answers!</span>
+            </h2>
 
-              {/* Tabs */}
-              <div className="flex justify-center gap-4 mb-10">
-                {[
-                  { label: "General", value: "general" },
-                  { label: "Registration", value: "registration" },
-                ].map((tab) => (
-                  <button
-                    key={tab.value}
-                    onClick={() => setActiveTab(tab.value)}
-                    className={`px-6 py-2 rounded-full border font-semibold text-sm transition ${
-                      activeTab === tab.value
-                        ? "bg-[#4285F4] text-white border-[#4285F4]"
-                        : "bg-white text-black border-gray-300 hover:bg-gray-100"
-                    }`}
-                  >
-                    {tab.label}
-                  </button>
-                ))}
-              </div>
-
-              {/* Q&A Section */}
-              <div className="rounded-2xl w-full p-6 md:p-10 text-start">
-                {currentQuestions.map((item, idx) => (
-                  <div key={idx} className="pb-6 my-6 w-full flex border-b border-gray-300">
-                    <h3 className="text-lg font-semibold w-5/12 text-black mb-2">
-                      {item.question}
-                    </h3>
-                    <p className="text-gray-600 text-sm w-7/12 leading-relaxed">
-                      {item.answer}
-                    </p>
-                    {idx < currentQuestions.length - 1 && <hr className="mt-6" />}
-                  </div>
-                ))}
-              </div>
+            {/* Tabs */}
+            <div className="flex justify-center gap-4 mb-6 sm:mb-10">
+              {[
+                { label: "General", value: "general" },
+                { label: "Registration", value: "registration" },
+              ].map((tab) => (
+                <button
+                  key={tab.value}
+                  onClick={() => setActiveTab(tab.value)}
+                  className={`px-4 py-2 rounded-full border font-semibold text-sm transition ${
+                    activeTab === tab.value
+                      ? "bg-[#4285F4] text-white border-[#4285F4]"
+                      : "bg-white text-black border-gray-300 hover:bg-gray-100"
+                  }`}
+                >
+                  {tab.label}
+                </button>
+              ))}
             </div>
-          </section>
-        </main>
+
+            {/* Q&A Section */}
+            <div className="rounded-2xl w-full p-4 md:p-10 text-start">
+              {currentQuestions.map((item, idx) => (
+                <div key={idx} className="pb-6 my-6 w-full flex flex-col md:flex-row border-b border-gray-300">
+                  <h3 className="text-lg font-semibold sm:w-full md:w-5/12 text-black mb-2">
+                    {item.question}
+                  </h3>
+                  <p className="text-gray-600 text-sm sm:w-full md:w-7/12 leading-relaxed">
+                    {item.answer}
+                  </p>
+                  {idx < currentQuestions.length - 1 && <hr className="mt-6" />}
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
+
         <div className="relative w-full min-h-screen bg-[#4147FF] text-white pt-[100px] pb-24 overflow-hidden">
         <div className="absolute top-0 left-0 w-full h-[25%] bg-gradient-to-b from-white to-transparent z-0" />
 
